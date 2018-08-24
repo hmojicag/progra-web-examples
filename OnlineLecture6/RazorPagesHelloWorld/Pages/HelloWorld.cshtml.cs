c4d52d930422331a1c6db6294d5b144647dd5eeb
hmojica <mojica.hazael@gmail.com>
2018-08-24 12:29:58 -0500

    Finishing up OnlineCourse 6 example

---
+++ OnlineLecture6/RazorPagesHelloWorld/Pages/HelloWorld.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesHelloWorld.Pages
+{
+    public class HelloWorldModel : PageModel {
+
+        public List<string> SongNames;
+
+        public void OnGet() {
+            SongNames = new List<string>();
+            SongNames.Add("Birth of Venus Illegitima");
+            SongNames.Add("The Invincible");
+            SongNames.Add("O Fortuna");
+            SongNames.Add("Raven of dispersion");
+        }
+    }
+}
