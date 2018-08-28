9bc4e1d6baa88bae7eab67b7423f93390de10931
hmojica <mojica.hazael@gmail.com>
2018-08-28 15:14:26 -0500

    OnlineLecture7 ready

---
+++ OnlineLecture7/src/Models/Song.cs
@@ -0,0 +1,9 @@
+﻿namespace RazorPagesHelloWorld.Models {
+    public class Song {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Album { get; set; }
+        public string Artist { get; set; }
+        public string AlbumCoverURL { get; set; }
+    }
+}
