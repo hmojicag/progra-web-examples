76b4a570869154c16efc5953b97823d339b83c85
hmojica <mojica.hazael@gmail.com>
2018-09-01 13:28:10 -0500

    This are not from this lecture

--- OnlineLecture9/src/Models/Album.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RazorPagesHelloWorld.Models {
-    public class Album {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public string Artist { get; set; }
-        public string AlbumCoverURL { get; set; }
-    }
-}
--- OnlineLecture9/src/Models/User.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RazorPagesHelloWorld.Models {
-    public class User {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public string UserName { get; set; }
-    }
-}
